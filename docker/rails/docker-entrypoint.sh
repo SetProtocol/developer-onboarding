38b47f9e995edca5f2483d2707b0835fb61bff75
Darlin Alberto <dalberto@users.noreply.github.com>
2021-06-09 19:45:33 -0400

    Mount SSH and gitconfigs for vendorizing support (#1)
    * Add OpenSSH and Git config, to allow vendorizing
    
    * Update bump-models command to account for stale filesystem

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -29,6 +29,10 @@ if [ "$1" = 'test' ]; then
 fi
 
 if [ "$1" = 'bump-models' ]; then
+  # Since the container filesystem may be out-of-date (see: http://docs.docker.oeynet.com/docker-for-mac/osxfs-caching/#cached), 
+  # force an update of the git index.
+  git status
+
   # Update private gem for new migrations and model logic
   exec rake vendorise:gem[git@github.com:SetProtocol/set-models.git]
 fi
