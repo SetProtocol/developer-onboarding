27d06d8e146ef3122b8993b35165e685d1fb6a45
williamsfu99 <32112201+williamsfu99@users.noreply.github.com>
2022-01-19 11:30:10 -0800

    Include branch option to release the current branch (#6)
    * Include branch option to release the current branch
    
    * Flatten options to make target and action mutually exclusive

--- release-helper.sh
+++ release-helper.sh
@@ -1,29 +1,53 @@
 #!/usr/bin/env sh
 
+# Usage:
+### Push HEAD to staging and staging-mainnet
+# developer-onboarding/release-helper.sh push-test
+#
+#
+### Push HEAD to all 3 stages
+# developer-onboarding/release-helper.sh push-production
+#
+#
+### Push the current branch, instead of HEAD, to staging and staging-mainnet
+# developer-onboarding/release-helper.sh push-branch
+
 set -ex
 
 action=$1
-option=$2
 
-if [[ "${action}" == "push" ]]; then
-  if [[ "${option}" == "test" ]]; then
-    git push staging master --no-verify
-    git push staging-mainnet master --no-verify
+repo_name=$(basename `git rev-parse --show-toplevel`)
 
-    read -p "Do you wish to also perform a DB migrate to staging/staging-mainnet? (y/n) " yn
-    case $yn in
-        [Yy]* ) heroku run rake db:migrate -a set-core-staging; heroku run rake db:migrate -a set-core-staging-mainnet; break;;
-        [Nn]* ) echo "Skipping.";;
-        * ) echo "Skipping.";;
-    esac
-  elif [[ "${option}" == "production" ]]; then
-    git push production master --no-verify
+if [[ "${action}" == "push-test" ]]; then
+  git push staging master --no-verify
+  git push staging-mainnet master --no-verify
 
-    read -p "Do you wish to also perform a DB migrate to production? (y/n) " yn
-    case $yn in
-        [Yy]* ) heroku run rake db:migrate -a set-core-production; break;;
-        [Nn]* ) echo "Skipping.";;
-        * ) echo "Skipping.";;
-    esac
-  fi
+  read -p "Do you wish to also perform a DB migrate to staging/staging-mainnet? (y/n) " yn
+  case $yn in
+      [Yy]* ) heroku run rake db:migrate -a $repo_name-staging; heroku run rake db:migrate -a $repo_name-staging-mainnet; break;;
+      [Nn]* ) echo "Skipping.";;
+      * ) echo "Skipping.";;
+  esac
+elif [[ "${option}" == "push-branch" ]]; then
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  echo "Pushing branch ${branch}:master to staging and staging-mainnet."
+
+  git push staging $branch:master
+  git push staging-mainnet $branch:master
+
+  read -p "Do you wish to also perform a DB migrate to staging/staging-mainnet? (y/n) " yn
+  case $yn in
+      [Yy]* ) heroku run rake db:migrate -a $repo_name-staging; heroku run rake db:migrate -a $repo_name-staging-mainnet; break;;
+      [Nn]* ) echo "Skipping.";;
+      * ) echo "Skipping.";;
+  esac
+elif [[ "${option}" == "push-production" ]]; then
+  git push production master --no-verify
+
+  read -p "Do you wish to also perform a DB migrate to production? (y/n) " yn
+  case $yn in
+      [Yy]* ) heroku run rake db:migrate -a $repo_name-production; break;;
+      [Nn]* ) echo "Skipping.";;
+      * ) echo "Skipping.";;
+  esac
 fi
